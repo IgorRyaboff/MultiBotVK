5c38aa846d7725801663f9ce87828062ab9a6657
root <root@mr.ryaboff2013.example.com>
2020-06-27 05:31:43 +0600

    Bugfixes, improved require function usage security

--- bin/app.js
+++ bin/app.js
@@ -2,27 +2,30 @@ const http = require('http');
 
 http.createServer((req, res) => {
     let complete = (text = 'ok', code = 200) => {
+        console.log(code, text);
         res.writeHead(code);
         res.end(text);
     }
-    if (req.method != 'POST') complete('MultiBotVK Error: POST method required', 403);
-
+    if (req.method != 'POST') return complete('MultiBotVK Error: POST method required, URL: ' + req.url, 403);
+    if (!req.url.replace('/', '').trim()) return complete('MultiBotVK Error: Empty URL', 404);
+    if (req.url.indexOf('.') != -1) return complete('MultiBotVK Error: URL cannot contain comma: ' + req.url);
     let script;
     try {
-        script = require('../scripts/' . req.url.replace('/', ''));
+        let path = require('path').resolve('./scripts/' + req.url.replace('/', ''));
+        script = require(path);
     }
     catch (e) {
-        return complete('MultiBotVK Error: Script ' + req.url.replace('/', '') + ' not found (or other require error)', 404);
+        return complete('MultiBotVK Error: Error requiring script ' + req.url.replace('/', '') + ': ' + e.message, 404);
     }
 
-    let input;
+    let input = '';
     req.on('data', chunk => input += chunk.toString());
     req.on('end', () => {
         try {
             input = JSON.parse(input);
         }
         catch (e) {
-            return complete('MultiBotVK Error: Could not parse JSON input', 500);
+            return complete('MultiBotVK Error: Could not parse JSON input: ' + input, 500);
         }
 
         script(input).then(result => {
