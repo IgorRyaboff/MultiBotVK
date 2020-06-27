80c2bb3b5b9c176658babf1665183e88bbd3b0a0
Igor Ryabov <mr.ryaboff2013@yandex.ru>
2020-06-27 23:29:33 +0600

    Added config.js mechanic, redirecting root (empty) URLs, script cache clear API

--- bin/app.js
+++ bin/app.js
@@ -1,21 +1,32 @@
 const http = require('http');
+const config = require('../config');
 
 http.createServer((req, res) => {
+    let url = req.url.replace('/', '').trim();
     let complete = (text = 'ok', code = 200) => {
         console.log(code, text);
-        res.writeHead(code);
+        res.statusCode = code;
         res.end(text);
     }
+    if (!url) {
+        if (!config.rootRedirect) return complete('MultiBotVK Error: Empty URL', 404);
+        res.setHeader('Location', config.rootRedirect);
+        complete('MultiBotVK: Redirected root', 301);
+    }
+    if (url.startsWith('!')) {
+        complete('MultiBotVK: Cleared require() cache for script ' + url.replace('!', '') + ' (if this script exists)');
+        delete require.cache[url.replace('!', '')];
+        return;
+    }
     if (req.method != 'POST') return complete('MultiBotVK Error: POST method required, URL: ' + req.url, 403);
-    if (!req.url.replace('/', '').trim()) return complete('MultiBotVK Error: Empty URL', 404);
     if (req.url.indexOf('.') != -1) return complete('MultiBotVK Error: URL cannot contain comma: ' + req.url);
     let script;
     try {
-        let path = require('path').resolve('./scripts/' + req.url.replace('/', ''));
+        let path = require('path').resolve('./scripts/' + url);
         script = require(path);
     }
     catch (e) {
-        return complete('MultiBotVK Error: Error requiring script ' + req.url.replace('/', '') + ': ' + e.message, 404);
+        return complete('MultiBotVK Error: Error requiring script ' + url + ': ' + e.message, 404);
     }
 
     let input = '';
